e3e3100be62960c29462e7fb7b125f83d398c34c
Ben Penner <ben.penner@boldcommerce.com>
2022-04-25 13:30:41 +0000

    HDLS-381 - Selected Language Support

--- templates/src/hooks/useGetDefaultSupportedLanguageIso.ts
+++ templates/src/hooks/useGetDefaultSupportedLanguageIso.ts
@@ -1,14 +1,20 @@
 import {displayFatalError} from 'src/utils';
 import {useDispatch} from 'react-redux';
-import {useGetAllSupportedLanguages} from 'src/hooks';
+import {useGetAllSupportedLanguages, useGetSelectedLanguageIso} from 'src/hooks';
 
 export function useGetDefaultSupportedLanguageIso(): string | null {
     const dispatch = useDispatch();
     const supportedLanguages = useGetAllSupportedLanguages();
+    const selectedLanguage = useGetSelectedLanguageIso();
+    let language;
     if(Array.isArray(supportedLanguages) && supportedLanguages.length > 0) {
-        let language = supportedLanguages.find(o => o.is_default === true);
+        // use selected language if it is supported
+        if (selectedLanguage) {
+            language = supportedLanguages.find(o => o.iso_language === selectedLanguage);
+        }
+        // if language isn't set use default
         if (!language) {
-            language = supportedLanguages[0];
+            language = supportedLanguages.find(o => o.is_default === true) || supportedLanguages[0];
         }
         return language.iso_language;
     } else {
