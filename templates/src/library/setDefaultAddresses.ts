6ccda73d954202e2ae7027850cb23177c61985bd
Kara Falkowski <kara.falkowski@boldcommerce.com>
2022-05-06 13:17:38 -0500

    HDLS-417: Validate addresses on resume buy now checkout

--- templates/src/library/setDefaultAddresses.ts
+++ templates/src/library/setDefaultAddresses.ts
@@ -4,30 +4,18 @@ import { Constants, defaultAddressState } from 'src/constants';
 import { actionUpdateAddress, actionUpdateBillingType, actionUpdateBillingTypeInSettings } from 'src/action';
 import { validateBillingAddress, validateShippingAddress } from '.';
 import { isObjectEquals } from 'src/utils';
-import { setShippingAddressAsValid } from './setShippingAddressAsValid';
-import { setBillingAddressAsValid } from './setBillingAddressAsValid';
 
 export async function setDefaultAddresses(dispatch: Dispatch, getState: () => IOrderInitialization): Promise<void> {
     const shippingAddress = getState().data.application_state.addresses.shipping;
-    const billingAddress = getState().data.application_state.addresses.billing;
-
     const savedAddresses = getState().data.application_state.customer.saved_addresses;
-
-    //if shipping address already set, we're resuming an order, shipping address is valid
-    if (!isObjectEquals(shippingAddress, defaultAddressState)) {
-        dispatch(setShippingAddressAsValid);
-    } 
-    //if billing address already set, we're resuming an order, billing address is valid
-    if (!isObjectEquals(billingAddress, defaultAddressState)) {
-        dispatch(setBillingAddressAsValid);
-    }
 
     if(isObjectEquals(shippingAddress, defaultAddressState) && savedAddresses.length > 0) {
         dispatch(actionUpdateAddress(Constants.SHIPPING, savedAddresses[0]));
-        await dispatch(validateShippingAddress);
 
         dispatch(actionUpdateBillingTypeInSettings(Constants.SHIPPING_SAME));
         dispatch(actionUpdateBillingType(Constants.SHIPPING_SAME, savedAddresses[0]));
-        await dispatch(validateBillingAddress);
     }
+
+    await dispatch(validateShippingAddress);
+    await dispatch(validateBillingAddress);
 }
