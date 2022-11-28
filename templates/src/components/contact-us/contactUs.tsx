bcb7c3112c6109b799b01162d22fbb1ef6f17be0
Ben Penner <ben.penner@boldcommerce.com>
2022-11-28 16:53:01 +0000

    [CP-290] - Contact us link new tab

--- templates/src/components/contact-us/contactUs.tsx
+++ templates/src/components/contact-us/contactUs.tsx
@@ -6,13 +6,13 @@ import {IContactUsProps} from 'src/types';
 
 export function ContactUs(props: IContactUsProps): React.ReactElement {
     const {needHelp, contactUs} = useGetContactUs();
-    const cssClass  = ClassNames(['contact-us', 'contact-us__need-help', props.className]);
+    const cssClass = ClassNames(['contact-us', 'contact-us__need-help', props.className]);
     const contactUsLink = `mailto:${window.supportEmail}`;
 
     return (
         <div className={cssClass}>
             {needHelp}
-            <a className={'contact-us__contact-link'} href={contactUsLink}>{` ${contactUs}`}</a>
+            <a className={'contact-us__contact-link'} href={contactUsLink} target="_blank">{` ${contactUs}`}</a>
         </div>
     );
 }
