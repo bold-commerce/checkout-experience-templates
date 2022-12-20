22f2e059cb7c7cf1cbd84a6380d32fd32cd57d41
Swann Lenik <swann.lenik@boldcommerce.com>
2022-12-20 21:56:46 +0000

    [CE-816] Session Expired wording Change

--- templates/src/constants/version.ts
+++ templates/src/constants/version.ts
@@ -1,1 +1,1 @@
-export const LIB_VERSION = '1.0.1-build.145';
+export const LIB_VERSION = '1.0.1-build.146';
--- templates/tests/component/session-expired/sessionExpired.test.tsx
+++ templates/tests/component/session-expired/sessionExpired.test.tsx
@@ -1,40 +1,33 @@
-import {render} from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
 import {mocked} from 'jest-mock';
 
 import {SessionExpired} from 'src/components';
 import {
     useGetContactUs,
     useGetFooterRights,
-    useGetSessionExpired,
     useGetShopUrlFromShopAlias,
     useScreenBreakpoints,
     useSupportedLanguages
 } from 'src/hooks';
 import {IUseContactUs, IUseFooterRights, IUseScreenBreakpoints, IUseSessionExpired} from 'src/types';
+import {getErrorTerm, getTerm} from 'src/utils';
 
-jest.mock('src/hooks/useGetSessionExpired');
 jest.mock('src/hooks/useGetContactUs');
 jest.mock('src/hooks/useGetFooterRights');
 jest.mock('src/hooks/useSupportedLanguages');
 jest.mock('src/hooks/useGetShopUrlFromShopAlias');
 jest.mock('src/hooks/useScreenBreakpoints');
+jest.mock('src/utils/getTerm');
+jest.mock('src/utils/getErrorTerm');
+const getTermMock = mocked(getTerm, true);
+const getErrorTermMock = mocked(getErrorTerm, true);
 const useScreenBreakpointsMock = mocked(useScreenBreakpoints, true);
-const useGetSessionExpiredMock = mocked(useGetSessionExpired, true);
 const useGetContactUsMock = mocked(useGetContactUs, true);
 const useGetFooterRightsMock = mocked(useGetFooterRights, true);
 const useSupportedLanguagesMock = mocked(useSupportedLanguages, true);
 const useGetShopUrlFromShopAliasMock = mocked(useGetShopUrlFromShopAlias, true);
 
 describe('testing SessionExpired component', () => {
-    const terms: Record<string, string> = {
-        returnToCart: 'return to cart',
-        sessionExpiredHeader: 'session expired header',
-        sessionExpiredBody: 'session expired body'
-    };
-    const hookReturn: IUseSessionExpired = {
-        returnUrl: jest.fn(),
-        terms
-    };
     const contactUsHookReturn: IUseContactUs = {
         needHelp: 'Need help?',
         contactUs: 'Contact us',
@@ -48,14 +41,23 @@ describe('testing SessionExpired component', () => {
         isTablet: true,
         isDesktop: false
     };
+    const shopUrl = 'test-shop.alias.com';
 
     beforeEach(() => {
         useScreenBreakpointsMock.mockReturnValue(mockScreenBreakpoints);
-        useGetSessionExpiredMock.mockReturnValue(hookReturn);
         useGetContactUsMock.mockReturnValue(contactUsHookReturn);
         useGetFooterRightsMock.mockReturnValue(footerRightsHookReturn);
         useSupportedLanguagesMock.mockReturnValue({languagesOptions: [], value: '', handleChange: jest.fn()});
         useGetShopUrlFromShopAliasMock.mockReturnValue('https://google.com');
+        getTermMock.mockReturnValue('some_text');
+        getErrorTermMock.mockReturnValue('some_error_text');
+        window = Object.create(window);
+        Object.defineProperty(window, 'location', {
+            value: {
+                href: 'http://dummy.com'
+            }
+        });
+        window.returnUrl = shopUrl;
     });
 
     test('Rendering SessionExpired component', () => {
@@ -63,5 +65,16 @@ describe('testing SessionExpired component', () => {
 
         expect(container.getElementsByClassName('session-expired__message').length).toBe(1);
         expect(container.getElementsByClassName('session-expired__footer-container').length).toBe(1);
+    });
+
+    test('Rendering SessionExpired component then click on Return to store button', () => {
+        const {container} = render(<SessionExpired />);
+        expect(container.getElementsByClassName('session-expired__message').length).toBe(1);
+        expect(container.getElementsByClassName('session-expired__footer-container').length).toBe(1);
+        expect(window.location.href).toBe('http://dummy.com');
+
+        const element = screen.getByTestId('footer-return-to-store-button') as HTMLButtonElement;
+        fireEvent.click(element);
+        expect(window.location.href).toBe(shopUrl);
     });
 });
