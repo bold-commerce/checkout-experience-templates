a185879f52fbc2d284ca6655f0cd8efbd5363d3d
Shagufa Ali <shagufa.ali@boldcommerce.com>
2022-12-21 15:43:16 +0000

    CE-828-Cart level discount issues

--- templates/src/library/validateDiscounts.ts
+++ templates/src/library/validateDiscounts.ts
@@ -1,19 +1,21 @@
 import {Dispatch} from 'redux';
 import {IError, IOrderInitialization} from 'src/types';
 import {deleteDiscounts, postDiscounts, validateDiscount} from 'src/library';
-import {errorFields, errorSeverities, errorSubTypes, errorTypes} from 'src/constants';
+import {errorFields, errorSeverities, errorSubTypes, errorTypes, PLUGIN_BACKEND_DISCOUNT_SOURCE} from 'src/constants';
 import {actionAddError} from 'src/action';
 
 export async function validateDiscounts(dispatch: Dispatch, getState: () => IOrderInitialization): Promise<void> {
     const {data: {application_state: {discounts}}} = getState();
     let isValidCode = true;
     for (const discount of discounts) {
-        await dispatch(deleteDiscounts(discount.code));
-        const response = await dispatch(validateDiscount(discount.code, false));
-        if(response.success){
-            await dispatch(postDiscounts(discount.code));
-        } else {
-            isValidCode = false;
+        if(discount.source !== PLUGIN_BACKEND_DISCOUNT_SOURCE) {
+            await dispatch(deleteDiscounts(discount.code));
+            const response = await dispatch(validateDiscount(discount.code, false));
+            if (response.success) {
+                await dispatch(postDiscounts(discount.code));
+            } else {
+                isValidCode = false;
+            }
         }
     }
 
