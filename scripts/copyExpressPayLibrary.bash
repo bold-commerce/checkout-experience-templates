d7fb50d63d8b6d5807099d3073aed820bc89a463
Felipe Dos Santos <felipe.dossantos@boldcommerce.com>
2023-02-14 11:23:17 -0600

    Add Scripts to Copy GitLab to GitHub

---
+++ scripts/copyExpressPayLibrary.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo Pulling Last GITLAB checkout-express-pay-library changes
+cd ~/repo/checkout-express-pay-library/
+git fetch
+git checkout develop
+git pull
+
+echo Pulling Last GITHUB checkout-express-pay-library changes
+cd ~/repo/github/checkout-express-pay-library/
+git fetch
+git checkout main
+git pull
+
+echo Rsync copy GITLAB to GITHUB folder of checkout-express-pay-library
+echo Rsync Excluding list: ~/repo/checkout-experience-templates/scripts/.rsyncignore
+cd ~/repo/github/
+rsync -av --exclude-from=./.rsyncignore ~/repo/checkout-express-pay-library/ ~/repo/github/checkout-express-pay-library/
