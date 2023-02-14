373765c63c734d3283eaa429953e0a3a007b6815
shagufa-ali <80478231+shagufa-ali@users.noreply.github.com>
2023-02-14 12:03:22 -0600

    Updating script folders.

--- scripts/copyExpressPayLibrary.bash
+++ scripts/copyExpressPayLibrary.bash
@@ -13,5 +13,5 @@ git pull
 
 echo Rsync copy GITLAB to GITHUB folder of checkout-express-pay-library
 echo Rsync Excluding list: ~/repo/checkout-experience-templates/scripts/.rsyncignore
-cd ~/repo/github/
+cd ~/repo/checkout-experience-templates/scripts/
 rsync -av --exclude-from=./.rsyncignore ~/repo/checkout-express-pay-library/ ~/repo/github/checkout-express-pay-library/
