37cb18dd91953f7a12ec7229f47943d015d467e1
Felipe dos Santos <felipe.dossantos@boldcommerce.com>
2023-10-11 13:01:38 -0500

    PXN-1245: Fix Timeout calculation and limit log string size on Meta Error

--- templates/src/themes/flow-sdk/meta/getTimerLog.ts
+++ templates/src/themes/flow-sdk/meta/getTimerLog.ts
@@ -2,7 +2,7 @@ import {META_TIMEOUT_SECONDS} from 'src/themes/flow-sdk/constants';
 
 export function getTimerLog(startTime: number): string {
     const endTime = Math.floor(Date.now() / 1000);
-    const elapsedTime = startTime - endTime;
+    const elapsedTime = endTime - startTime;
     const isTimeout = elapsedTime > META_TIMEOUT_SECONDS ? 'TIMEOUT' : '';
 
     return `${isTimeout} elapsedTime: ${elapsedTime.toFixed(3)}s endTime: ${endTime.toFixed(3)}s`;
