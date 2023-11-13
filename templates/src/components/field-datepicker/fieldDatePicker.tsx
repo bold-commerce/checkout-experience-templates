2cf14a72b238274e4afe85a2ee98751e6a867552
Zhining Liu <zhining.liu@boldcommerce.com>
2023-11-13 12:46:57 -0600

    Remove bevy library

--- templates/src/components/field-datepicker/fieldDatePicker.tsx
+++ templates/src/components/field-datepicker/fieldDatePicker.tsx
@@ -4,8 +4,6 @@ import {FieldInput} from 'src/components';
 import TinyDatePicker from 'tiny-date-picker';
 import 'tiny-date-picker/tiny-date-picker.css';
 import {useGetAppSettingData, useGetDatePickerLanguage} from 'src/hooks';
-import {Calendar} from '@bold-commerce/bevy-react-icons';
-import {Icon} from '@bold-commerce/bevy-react';
 
 export function FieldDatePicker(props: IFieldDatePickerProps): React.ReactElement {
 
@@ -50,7 +48,7 @@ export function FieldDatePicker(props: IFieldDatePickerProps): React.ReactElemen
                 data-testid={`${props.id}-date-picker-input`}
                 errorMessage={props.errorMessage}
                 readonly={true}
-                prefix={<Icon source={Calendar} title='Calendar' aria-label='calendar' size='18' color='#1A1A1A'/>}
+                prefix={<span className='date-picker-icon'></span>}
             />
         </div>
     );
